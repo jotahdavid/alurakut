7023c5ecd356162fce33d34689b766f3030c3468
Jhonatan David <82552187+jotahdavid@users.noreply.github.com>
2021-07-12 12:09:44 -0300

    :sparkles: feat: set styles base

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,20 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
-  body {
+  * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+  }
+
+  html, body, #__next {
+    width: 100%;
+    height: 100%;
+  }
+
+  body {
+    background-color: #D9E6F6;
+    font-family: sans-serif;
   }
 `
 
