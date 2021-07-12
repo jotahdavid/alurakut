dfbb8f9ca0c7a4562d50af50a812b06e4572292a
Jhonatan David <82552187+jotahdavid@users.noreply.github.com>
2021-07-12 14:13:42 -0300

    :recycle: refactor: modularize components into files

--- pages/index.js
+++ pages/index.js
@@ -1,58 +1,21 @@
-import styled from 'styled-components'
+import MainGrid from '../src/components/MainGrid';
+import Box from '../src/components/Box';
 
-// const Title = styled.h1`
-//   font-size: 50px;
-//   color: ${({ theme }) => theme.colors.primary};
-// `
-
-const MainGrid = styled.main`
-  width: 100%;
-  max-width: 500px;
-  margin: 0 auto;
-  padding: 0 16px;
-  gap: 10px;
-
-  .profile-area {
-    display: none;
-
-    @media(min-width: 860px) {
-      display: block;
-      grid-area: "profileArea";
-    }
-  }
-
-  .welcome-area {
-    @media(min-width: 860px) {
-      grid-area: "welcomeArea";
-    }
-  }
-
-  .profile-relations-area {
-    @media(min-width: 860px) {
-      grid-area: "profileRelationsArea";
-    }
-  }
-
-  @media(min-width: 860px) {
-    max-width: 1110px;
-    display: grid;
-    grid-template-areas: "profileArea welcomeArea profileRelationsArea";
-    grid-template-columns: 160px 1fr 312px;
-  }
-`;
-
-const Box = styled.div`
-  background-color: #fff;
-  border-radius: 8px;
-`;
+function ProfileSidebar({ username }) {
+  return (
+    <Box>
+      <img src={`https://github.com/${username}.png`} style={{borderRadius: "8px"}} />
+    </Box>
+  );
+}
 
 export default function Home() {
+  const githubUser = "omariosouto";
+
   return (
     <MainGrid>
       <section className="profile-area">
-        <Box>
-          Imagem
-        </Box>
+        <ProfileSidebar username={githubUser} />
       </section>
       <section className="welcome-area">
         <Box>
---
+++ src/components/MainGrid/index.js
@@ -0,0 +1,39 @@
+import styled from 'styled-components';
+
+const MainGrid = styled.main`
+  width: 100%;
+  max-width: 500px;
+  margin: 0 auto;
+  padding: 16px;
+  gap: 10px;
+
+  .profile-area {
+    display: none;
+
+    @media(min-width: 860px) {
+      display: block;
+      grid-area: "profileArea";
+    }
+  }
+
+  .welcome-area {
+    @media(min-width: 860px) {
+      grid-area: "welcomeArea";
+    }
+  }
+
+  .profile-relations-area {
+    @media(min-width: 860px) {
+      grid-area: "profileRelationsArea";
+    }
+  }
+
+  @media(min-width: 860px) {
+    max-width: 1110px;
+    display: grid;
+    grid-template-areas: "profileArea welcomeArea profileRelationsArea";
+    grid-template-columns: 160px 1fr 312px;
+  }
+`;
+
+export default MainGrid;
