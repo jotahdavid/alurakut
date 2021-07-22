4bed79f980774a7676a5c2229e9a450c2ae058e2
Jhonatan David <82552187+jotahdavid@users.noreply.github.com>
2021-07-22 18:13:13 -0300

    :sparkles: feat: add logout system

---
+++ pages/logout.js
@@ -0,0 +1,20 @@
+import nookies from "nookies";
+
+export default function LogoutScreen() {
+  return (
+    <h1>Logout</h1>
+  );
+}
+
+export async function getServerSideProps(context) {
+  nookies.destroy(context, "USER_TOKEN", {
+    path: "/"
+  });
+
+  return {
+    redirect: {
+      destination: "/login",
+      permanent: false
+    }
+  };
+}
